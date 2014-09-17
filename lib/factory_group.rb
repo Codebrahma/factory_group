3c72f795c8eaac75ec343204679366d0235e6201
Yuvaraja Balamurugan <yuv.slm@gmail.com>
2014-09-17 23:07:36 +0530

    Storing all the defined factory into a registry

--- lib/factory_group.rb
+++ lib/factory_group.rb
@@ -2,7 +2,14 @@ require "factory_group/version"
 require "factory_group/group"
 
 module FactoryGroup
+  @registry = {}
+
+  def self.registry
+    @registry
+  end
+
   def self.define(name, &block)
-    Group.new.instance_eval(&block)
+    group = Group.new
+    FactoryGroup.registry[name] = group.instance_eval(&block)
   end
 end
