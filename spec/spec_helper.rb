09e0007c8f2a68da3f4c98633d61d8a3b12b4764
Yuvaraja Balamurugan <yuv.slm@gmail.com>
2014-09-20 20:54:02 +0530

    Moved the CodeClimate test reporter to the top of the spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
+
 $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 $LOAD_PATH << File.join(File.dirname(__FILE__))
 
@@ -6,9 +10,6 @@ require 'rspec'
 require 'debugger'
 
 require 'factory_group'
-require "codeclimate-test-reporter"
-
-CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   
